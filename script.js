ea928da5ffee4cd4157b9fdb3dd5baa9e16c474c
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-03-20 15:16:53 +0200

    Create main block

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let prevScrollPos = window.scrollY;
+// let prevScrollPos = window.scrollY;
 
-window.onscroll = function () {
+// window.onscroll = function () {
+//   let currentScrollPos = window.scrollY;
+//   if (prevScrollPos > currentScrollPos) {
+//     document.querySelector(".header").style.top = "0";
+//   } else {
+//     document.querySelector(".header").style.top = "-75px";
+//   }
+//   prevScrollPos = currentScrollPos;
+// };
+
+let prevScrollPos = window.scrollY;
+const scrollHandle = () => {
+  console.log("hello");
   let currentScrollPos = window.scrollY;
   if (prevScrollPos > currentScrollPos) {
     document.querySelector(".header").style.top = "0";
   } else {
-    document.querySelector(".header").style.top = "-70px"; // Adjust according to header height
+    document.querySelector(".header").style.top = "-75px";
   }
   prevScrollPos = currentScrollPos;
 };
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+document.querySelector(".back-to-top").addEventListener("click", scrollToTop);
+
+window.onscroll = scrollHandle;
