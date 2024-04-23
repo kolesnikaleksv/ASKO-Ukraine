507ee24a4d50bc1a22af655e51ab04ad926089f0
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-04-23 11:56:02 +0300

    Add readme.md file, and folder for min and unmin files

---
+++ README.md
@@ -0,0 +1,7 @@
+# ACKO-Ukraine
+
+## How To Use
+
+All non-minimized files (index.css, index.js, index.html) are saved in the 'un.min.src_files' folder.
+
+All minimized files are saved in the "minimized_files" folder.
---
+++ un.min.src_files/script.js
@@ -0,0 +1,53 @@
+let prevScrollPos = window.scrollY;
+const scrollHandle = () => {
+  let currentScrollPos = window.scrollY;
+  if (prevScrollPos > currentScrollPos) {
+    document.querySelector(".header").style.top = "0";
+  } else {
+    document.querySelector(".header").style.top = "-75px";
+  }
+  prevScrollPos = currentScrollPos;
+};
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+function toggleMenu() {
+  if(this.classList.contains('active')) {
+    this.classList.remove('active');
+    document.querySelector('.header').classList.remove('active');
+    document.querySelector('.close-icon').style.display = 'none';
+    document.querySelector('.open-icon').style.display = 'block';
+    document.querySelector('.menu-overlay').style.display = 'none';
+    document.getElementById('body').classList.remove('stop-scrolling');
+    document.querySelector('.navbar-brand').classList.remove('menu-active');
+  } else {
+    this.classList.add('active');
+    document.querySelector('.header').classList.add('active');
+    document.querySelector('.header').classList.remove('hover');
+    document.querySelector('.close-icon').style.display = 'block';
+    document.querySelector('.open-icon').style.display = 'none';
+    document.querySelector('.menu-overlay').style.display = 'flex';
+    document.getElementById('body').classList.add('stop-scrolling');
+    document.querySelector('.navbar-brand').classList.add('menu-active');
+  }
+};
+
+document.querySelector(".back-to-top").addEventListener("click", scrollToTop);
+document.querySelector(".btn-menu").addEventListener("click", toggleMenu)
+window.onscroll = scrollHandle;
+
+const swiper = new Swiper('.fool-screen-slider', {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
